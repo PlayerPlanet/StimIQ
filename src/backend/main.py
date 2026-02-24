5a37f7a09a78f733ae490f9122c8380dc92ab287
Konsta Kiirikki <93705122+PlayerPlanet@users.noreply.github.com>
2026-02-24 06:22:42 +0100

    Update main.py
    added the bought url to allowed origins

--- src/backend/main.py
+++ src/backend/main.py
@@ -43,6 +43,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://127.0.0.1:5175",
         "https://stim-iq.vercel.app",
+        "stimiq.xyz",
     ],
     allow_credentials=True,
     allow_methods=["*"],
